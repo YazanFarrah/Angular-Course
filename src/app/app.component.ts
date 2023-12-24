bbcff3828db006733ed915bb4929130a1f7aa431
YazanFarrah <yazanfarrah@gmail.com>
2023-12-24 19:15:25 +0300

    Lifecycle Hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   serverElements = [{ type: 'server', name: 'TestServer', content: 'A test' }];
 
   onServerAdded(serverData: { serverName: string; serverContent: string }) {
@@ -26,5 +25,13 @@ export class AppComponent {
       name: blueprintData.serverName,
       content: blueprintData.serverContent,
     });
+  }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed!';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
   }
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,11 +1,68 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrl: './server-element.component.css',
 })
-export class ServerElementComponent {
-  //binding through alias to use this name instead of element if I wanted'srvElement'
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
   @Input('srvElement') element: { type: string; name: string; content: string };
+  @Input() name: string;
+  constructor() {
+    console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called');
+  }
 }
